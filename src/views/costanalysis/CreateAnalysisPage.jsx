9c6fa82f554a7f24b09ec22d328e3601656c5905
Kerlly Alexandra Chiriboga Cabrera <chiribogakerly@gmail.com>
2026-01-16 00:35:28 -0500

    save steps

--- src/views/costanalysis/CreateAnalysisPage.jsx
+++ src/views/costanalysis/CreateAnalysisPage.jsx
@@ -73,7 +73,8 @@ const CreateAnalysisPage = ({ onBack, onSuccess }) => {
           unit: i.selectedUnit,
         }))
       );
-      setIngredientsCostResult(response);
+
+      setIngredientsCostResult(response.data);
       setStep(2);
     } catch {
       showToast("Error al calcular ingredientes", "error");
@@ -89,9 +90,10 @@ const CreateAnalysisPage = ({ onBack, onSuccess }) => {
         servings: Number(selectedRecipe?.servings) || 1,
         margin: 30,
       });
-      console.log("BACKEND productCostResult", response);
 
-      setProductCostResult(response);
+      console.log("BACKEND productCostResult 👉", response.data);
+
+      setProductCostResult(response.data);
       setStep(3);
     } catch (error) {
       console.error(error);
@@ -257,32 +259,22 @@ const CreateAnalysisPage = ({ onBack, onSuccess }) => {
 
               {step === 4 && (
                 <>
-                  {!taxesResult && (
-                    <p className="text-gray-500">Calculando impuestos...</p>
-                  )}
+                  <p>
+                    Impuestos: $
+                    {(Number(taxesResult?.taxes?.totalTaxes) || 0).toFixed(2)}
+                  </p>
 
-                  {taxesResult?.taxes && (
-                    <>
-                      <p>
-                        Impuestos: $
-                        {(Number(taxesResult.taxes.totalTaxes) || 0).toFixed(2)}
-                      </p>
+                  <p>
+                    Precio final: $
+                    {(Number(taxesResult?.finalPrice) || 0).toFixed(2)}
+                  </p>
 
-                      <p>
-                        Precio final: $
-                        {(Number(taxesResult.finalPrice) || 0).toFixed(2)}
-                      </p>
-
-                      <button
-                        onClick={() =>
-                          setConfirm({ open: true, loading: false })
-                        }
-                        className="mt-6 bg-green-600 text-white px-6 py-2 rounded-md"
-                      >
-                        Guardar análisis
-                      </button>
-                    </>
-                  )}
+                  <button
+                    onClick={() => setConfirm({ open: true, loading: false })}
+                    className="mt-6 bg-green-600 text-white px-6 py-2 rounded-md"
+                  >
+                    Guardar análisis
+                  </button>
                 </>
               )}
             </>
